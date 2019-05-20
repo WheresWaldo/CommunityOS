be09b092d2b5fc51dabd707f27d8efbad65cc76a
mark tomlinson <marktoml@hotmail.com>
2019-05-20 17:57:47 -0400

    Main dialogs/icons completed

---
+++ Robo Gui/src/robo/gui/ButtonEditor.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2019 markt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package robo.gui;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author markt
+ */
+class ButtonEditor extends DefaultCellEditor {
+
+    private static final long serialVersionUID = 1L;
+  protected JButton button;
+
+  private String label;
+
+  private boolean isPushed;
+
+  public ButtonEditor(JCheckBox checkBox) {
+    super(checkBox);
+    button = new JButton();
+    button.setOpaque(true);
+    button.addActionListener((ActionEvent e) -> {
+        fireEditingStopped();
+    });
+  }
+
+    @Override
+  public Component getTableCellEditorComponent(JTable table, Object value,
+      boolean isSelected, int row, int column) {
+    if (isSelected) {
+      button.setForeground(table.getSelectionForeground());
+      button.setBackground(table.getSelectionBackground());
+    } else {
+      button.setForeground(table.getForeground());
+      button.setBackground(table.getBackground());
+    }
+    label = (value == null) ? "" : value.toString();
+    button.setText(label);
+    isPushed = true;
+    return button;
+  }
+
+    @Override
+  public Object getCellEditorValue() {
+    if (isPushed) {
+      // 
+      // 
+      JOptionPane.showMessageDialog(button, label + ": Ouch!");
+      // System.out.println(label + ": Ouch!");
+    }
+    isPushed = false;
+    return label;
+  }
+
+    @Override
+  public boolean stopCellEditing() {
+    isPushed = false;
+    return super.stopCellEditing();
+  }
+
+  protected void fireEditingStopped() {
+    super.fireEditingStopped();
+  }
+}
